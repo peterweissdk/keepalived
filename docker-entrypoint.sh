8494b91968bf53f48bd8de1bb3e8e888d93dd2d9
peterweissdk <peterweissdk@gmail.com>
2025-07-07 13:04:26 +0200

    feat: add support for multiple unicast peers in keepalived configuration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ if [ -n "$TZ" ]; then
     echo "$TZ" > /etc/timezone
 fi
 
+# Format the UNICAST_PEERS for proper indentation
+export UNICAST_PEERS=$(echo "$UNICAST_PEERS" | tr ',' '\n' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g' | sed 's/^/        /')
+
 # Generate keepalived configuration from template
 envsubst < /conf/keepalived.conf_tpl > /etc/keepalived/keepalived.conf
 
